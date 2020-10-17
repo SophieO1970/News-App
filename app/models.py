4d2c61ad6ad00f976db9918e79f9eee9b9e81cdb
sophieO1970 <sophieO1970@gmail.com>
2020-10-17 20:33:59 +0300

    add article class that define the articles objects

--- app/models.py
+++ app/models.py
@@ -7,3 +7,16 @@ class Sources:
         self.description = description
         self.url = url
         self.category = category
+ 
+
+ 
+class Articles:
+    """ Articles class to define the articles object """
+
+    def __init__(self, author, title, description, urlToImage, url, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.url = url
+        self.publishedAt = publishedAt       
